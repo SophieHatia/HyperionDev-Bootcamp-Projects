9326e83ab0722d7629fedf8bbc9bb58a1466a01f
Sophie Hatia <162351695+SophieHatia@users.noreply.github.com>
2024-03-12 15:04:09 +0000

    Update award.py

--- award.py
+++ award.py
@@ -1,12 +1,16 @@
+#take inputs for each sport time for the user
 CyclingTime= input("Time taken to complete cycling section= ")
 RunningTime = input("Time taken to complete running section= ")
 SwimmingTime = input("Time taken to complete swimming section= ")
 
+#find the totatl time of the triathlon by summing the three times, converting each input to a float from a string
 TotalTime = float(CyclingTime)+float(RunningTime)+float(SwimmingTime)
 print(TotalTime)
 
+#finding the difference in time from the minimum time for an award
 TimeDiff = TotalTime - 100
 
+#creating if statements to check the time difference to the award given for the times
 if TimeDiff <= 0:
     print("You have been awarded Provincial Colours ")
 elif 1<= TimeDiff and TimeDiff <= 5:
